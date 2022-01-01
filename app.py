850999dad810e75f246e6c3c8d787beb47b68a13
dhravya <dhravyashah@gmail.com>
2022-01-01 18:48:47 +0000

    did some good changes

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_file
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/resume")
+def resume():
+    return send_file('static/Dhravyas_Resume.pdf', attachment_filename='resume.pdf')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./templates/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        gatwick: "Gatwick"
+    }
+  },
+  plugins: [],
+}
+}
