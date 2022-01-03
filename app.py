c956782d296721d4e4287d615f1b0199c4654431
Dhravya <dhravyashah@gmail.com>
2022-01-03 21:35:19 +0530

    added mask detector

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, Response 
+from mask_detector import detector
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +15,13 @@ def resume():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(detector.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/mask-detector')
+def mask_detector():
+    return render_template('mask_detector.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
- 
+    app.run(debug=True)
